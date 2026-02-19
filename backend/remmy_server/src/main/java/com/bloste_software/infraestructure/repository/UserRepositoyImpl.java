3bb913c70c089bc3cacc4923e55af589c3bf567b
Toxolo <josepsmorant@gmail.com>
2026-02-19 08:23:18 +0100

    Login p2

--- backend/remmy_server/src/main/java/com/bloste_software/infraestructure/repository/UserRepositoyImpl.java
+++ backend/remmy_server/src/main/java/com/bloste_software/infraestructure/repository/UserRepositoyImpl.java
@@ -1,5 +1,46 @@
 package com.bloste_software.infraestructure.repository;
 
-public class UserRepositoyImpl {
+import java.util.List;
+
+import com.bloste_software.domain.repository.UserRepository;
+import com.bloste_software.presentation.dtos.UserDTO;
+
+public class UserRepositoyImpl implements UserRepository {
+
+    @Override
+    public List<UserDTO> findAllDTO() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllDTO'");
+    }
+
+    @Override
+    public void save(UserDTO user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void deleteById(String email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public void saveAll(List<UserDTO> users) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveAll'");
+    }
+
+    @Override
+    public void existsByEmail(String email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'existsByEmail'");
+    }
+
+    @Override
+    public void findByEmail(String email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByEmail'");
+    }
     
 }
