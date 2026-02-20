5421eccbfd6e67e1e370a3f3c71eb7958ff4beba
Toxolo <josepsmorant@gmail.com>
2026-02-20 08:12:40 +0100

    proba get

--- backend/remmy_server/src/main/java/com/bloste_software/domain/models/entities/users/User.java
+++ backend/remmy_server/src/main/java/com/bloste_software/domain/models/entities/users/User.java
@@ -1,12 +1,20 @@
 package com.bloste_software.domain.models.entities.users;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
--- backend/remmy_server/src/main/java/com/bloste_software/domain/repository/CenterRepository.java
+++ backend/remmy_server/src/main/java/com/bloste_software/domain/repository/CenterRepository.java
@@ -1,28 +1,28 @@
-package com.bloste_software.domain.repository;
+// package com.bloste_software.domain.repository;
 
-import java.util.List;
+// import java.util.List;
 
-import com.bloste_software.presentation.dtos.CenterDTO;
+// import com.bloste_software.presentation.dtos.CenterDTO;
 
-public interface CenterRepository {
+// public interface CenterRepository {
 
-    // Devuelve todos los usuarios
-    List<CenterDTO> getAllCenters();
+//     // Devuelve todos los usuarios
+//     List<CenterDTO> getAllCenters();
 
-    // Guardar centro
-    void save(CenterDTO center);
+//     // Guardar centro
+//     void save(CenterDTO center);
 
-    // Buscar por id
-    CenterDTO findById(Long id);
+//     // Buscar por id
+//     CenterDTO findById(Long id);
 
-    // Eliminar por id
-    void deleteById(Long id);
+//     // Eliminar por id
+//     void deleteById(Long id);
 
-    // Editar por id
-    void editById(Long id);
+//     // Editar por id
+//     void editById(Long id);
 
-    // Verificar si existe
-    boolean existsById(Long id);
+//     // Verificar si existe
+//     boolean existsById(Long id);
     
-}
+// }
     
